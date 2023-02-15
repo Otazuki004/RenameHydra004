9c73bb4c22df406a67f882e2da51dad1bd3ad9ea
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-02-15 23:21:20 +0530

    Update __init__.py

--- Hydra1/__init__.py
+++ Hydra1/__init__.py
@@ -10,13 +10,13 @@ logging.basicConfig(level=logging.INFO, format=FORMAT)
 OWNER_ID = 1985665341
 PREFIX = ["/","!","*","$","#","?",">",]
 
-mongo = pymongo.MongoClient("mongodb+srv://nandhaxd:rw5T7YJRjsE3fmk3@cluster0.80igexg.mongodb.net/?retryWrites=true&w=majority")
+mongo = pymongo.MongoClient("mongodb+srv://Otazuki_004:<password>@hydra.od82ic1.mongodb.net/?retryWrites=true&w=majority")
 db = mongo["Hydra1"]
 
 ND = Client("Hydra1",
      api_id=int(10187126),
      api_hash="ff197c0d23d7fe54c89b44ed092c1752",
-     bot_token="5401792933:AAHiYn6FBaD4GpD-1zlJgxpM5nR1Ou2kM-c",
+     bot_token="5401792933:AAFhqJuX2veBDEB5sNJnWCCW0Fp_czoXzbo",
      plugins=dict(root="Hydra1"),)
 
 
